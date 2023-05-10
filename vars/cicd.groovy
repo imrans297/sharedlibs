d1105f577276e27e3c28449d37d910db85f27852
imrans297 <57826758+imrans297@users.noreply.github.com>
2023-05-10 16:09:54 +0530

    commit a

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,20 +2,3 @@ def newGit(repo)
 {
   git "https://github.com/intelliqittrainings/${repo}"
 }
-
-def newMaven
-{
-  sh 'mvn package' 
-} 
-def newDeploy("jobname","ipaddress","context")
-{
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war"
-} 
-def runSelenium(jobname)
-{
-  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
-}  
-def newApproval(name)
-{
-   input message: 'Waiting for Approval', submitter: "${name}"  
-} 
